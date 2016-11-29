2a4ee301858c63c9f9dbb263e9d3890342ef62a2
David Laing <david.laing@alumni.ubc.ca>
2016-11-29 13:43:27 -0800

    added standard error function

--- R/standard_deviation.R
+++ R/standard_deviation.R
@@ -1,10 +1,11 @@
 #' Get the standard deviation of a vector
-#' 
+#'
 #' @param x a numeric vector
 #' @return The standard deviation of the numeric vector
-#' 
+#'
 #' @examples
 #' standard_deviation(c(1, 2, 3))
+#' 0.8164966
 #' @export
 standard_deviation <- function(x) {
   n <- length(x)
@@ -13,3 +14,13 @@ standard_deviation <- function(x) {
   stddev = sqrt(ssq/n)
   return(stddev)
 }
+
+#' Calculate the standard error of a vector
+#' @param x: a numeric vector
+#' @return: The standard error
+#'
+#' @examples
+#' standard_error(c(1, 2, 3))
+#' 0.4714045
+#' @export
+standard_error <- function(x) standard_deviation(x)/sqrt(length(x))
